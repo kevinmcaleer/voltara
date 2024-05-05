53088912c1dedac0c46dbc835eb4937ccbafea58
Kevin McAleer <kevinmcaleer@gmail.com>
2024-05-05 18:33:09 +0100

    added button press

--- voltara.py
+++ voltara.py
@@ -4,21 +4,27 @@ from Adafruit_Thermal import *
 
 printer = Adafruit_Thermal("/dev/serial0", 19200, timeout=5)
 printer.setTimes(35000, 2100)
-printer.feed(2)
+# printer.feed(2)
 
-button.wait_for_press()
+# printer.testPage()
 
-printer.feed(2)
+while  KeyboardInterrupt:
+    button.wait_for_press()
 
-printer.underlineOn()
-printer.setSize("M")
-printer.justify('C')
-printer.print(' --= Voltara Fortune Teller =--\n\n')
-printer.underlineOff()
-printer.setSize("S")
-printer.justify("L")
-printer.feed(2)
-printer.print("Your Wish is granted")
-printer.feed(2)
-printer.print("----------------------------------")
-printer.print("https://www.kevsrobots.com/voltara")
+    printer.feed(5)
+
+    printer.underlineOn()
+    printer.setSize("M")
+    printer.justify('C')
+    printer.print(' --= Voltara Fortune Teller =--\n\n')
+    printer.underlineOff()
+    printer.setSize("S")
+    printer.justify("L")
+    printer.feed(2)
+    printer.print("Your Wish is granted")
+    printer.feed(2)
+    printer.print("----------------------------------")
+    printer.print("https://www.kevsrobots.com/voltara")
+
+    printer.feed(4)
+    printer.reset()
